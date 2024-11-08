241a2e0375c5ab6340ab806809d727c4659ac698
Pergo911 <probsztgeri@gmail.com>
2024-11-08 23:14:34 +0100

    implement initial playlist fetch

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System.Text.Json;
 
-ConfigObject config = JsonSerializer.Deserialize<ConfigObject>(File.ReadAllText("config.json")) ?? throw new Exception("Nincs config fájl.");
+ConfigObject config = JsonSerializer.Deserialize<ConfigObject>(File.ReadAllText("D:\\Libraries\\Documents\\_Programming\\mark_yt_automation\\reciever-dotnet-2\\config.json")) ?? throw new Exception("Nincs config fájl.");
 var playlist_id = config.PLAYLIST_ID;
 var outputDirectory = config.OUTPUT_DIR;
 var apiKey = config.YOUTUBE_API_KEY;
 
+using var client = new HttpClient();
+
 // Checks if playlist contains any videos, downloads them and tags them
-async void MainLoop()
+async Task MainLoop()
 {
-    var url = $"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId={playlist_id}&key={apiKey}";
+    var url = $"https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId={playlist_id}&key={apiKey}";
+
+    var response = await client.GetAsync(url);
+
+    if (!response.IsSuccessStatusCode)
+    {
+        throw new Exception($"Failed to fetch playlist: {response.StatusCode}\n{await response.Content.ReadAsStringAsync()}");
+    }
+
+    var json = await response.Content.ReadAsStringAsync();
+
+    var playlist = JsonSerializer.Deserialize<Playlist>(json) ?? throw new Exception("Failed to parse JSON.");
+
+    if (playlist.items.Count == 0)
+    {
+        // TODO: remove to prevent spam
+        Console.WriteLine("No videos found in playlist.");
+        return;
+    }
+
+    foreach (var item in playlist.items)
+    {
+        Console.WriteLine(item.contentDetails.videoId);
+    }
 }
+
+await MainLoop();
--- Types.cs
+++ Types.cs
@@ -4,3 +4,18 @@ public class ConfigObject
   public required string OUTPUT_DIR { get; set; }
   public required string YOUTUBE_API_KEY { get; set; }
 }
+
+public class Playlist
+{
+  public required List<PlaylistItem> items { get; set; }
+}
+
+public class PlaylistItem
+{
+  public required ContentDetails contentDetails { get; set; }
+}
+
+public class ContentDetails
+{
+  public required string videoId { get; set; }
+}
